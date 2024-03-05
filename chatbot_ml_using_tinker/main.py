e6ab66881d85d072de6af4557477f55a308ed26e
milanr21 <milanrawal309@gmail.com>
2024-03-05 22:14:50 +0545

    chat updated

--- chatbot_ml_using_tinker/main.py
+++ chatbot_ml_using_tinker/main.py
@@ -1,6 +1,20 @@
 import tkinter as tk
-from tkinter import RIGHT, Tk, Button, Label, Entry, Text, Frame, Scrollbar, VERTICAL, Canvas, RAISED, Y
+from tkinter import RIGHT, Tk, Button, Label, Entry, Text, Frame, Scrollbar, VERTICAL, Canvas, RAISED, Y, END, StringVar, ttk
 import random
+import openai
+import os
+
+from dotenv import load_dotenv
+import openai
+
+
+
+
+
+load_dotenv()
+
+
+
 
 
 class chatbot:
@@ -8,6 +22,7 @@ class chatbot:
         self.root = root
         self.root.geometry('700x500+300+100')
         self.root.title('Intelligent Chatbot')
+        self.root.bind('<Return>', self.ent_func)
 
 
 
@@ -36,26 +51,60 @@ class chatbot:
 
         # search entry
 
-        self.entry = Entry(self.root, width=60, font=('Times New Roman', 14, 'bold'))
+        self.ent = StringVar()
+
+        self.entry = Entry(self.root, textvariable=self.ent, width=50, font=('Times New Roman', 14, 'bold'))
         self.entry.place(x=140, y=430)
 
 
         # creating button
 
-        self.btn_search = Button(self.root, text="Search", width=10, font=('Times New Roman', 14, 'bold'))
+        self.btn_search = Button(self.root, command=self.search, text="Search", width=10, font=('Times New Roman', 14, 'bold'))
         self.btn_search.place(x=150, y=460, height=30, width=100)
 
-        self.btn_clear = Button(self.root, text="Clear", width=10, font=('Times New Roman', 14, 'bold'))
+        self.btn_clear = Button(self.root, command=self.clear, text="Clear", width=10, font=('Times New Roman', 14, 'bold'))
         self.btn_clear.place(x=290, y=460, height=30, width=100)  # Use self.btn_clear.place instead of self.btn_search.place
 
 
-       
+# function to search the query
+        
+    def ent_func(self, event):
+        self.btn_search.invoke()
+        self.ent.set("")
+        
+        
+    def search(self):
+        user_input = "  " + "You: " + self.entry.get().lower()
+        self.text.insert(END, "\n\n" + user_input)
+
+        input_text = self.entry.get()
+
+        api_key = os.getenv("myAPI")
+
+        openai.api_key = api_key
+
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=input_text,
+            max_tokens=100
+        )
+
+        try:
+            msg_reply = response["choices"][0]["text"]
+            self.text.insert(END, "\n\n" + "Bot: " + msg_reply)
+        except (KeyError, IndexError):
+            self.text.insert(END, "\n\n" + "Bot: No response")
 
         
-
-
-      
+                      
         
+  
+
+    def clear(self):
+        self.text.delete('1.0', END)
+        self.ent.set("")
+
+
 
 
 
